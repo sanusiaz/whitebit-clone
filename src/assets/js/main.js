7f441970aefbe990ce6e92367c98e8b2d1a2a52e
sanusiaz <sanusiabdulazeezadebayo1@gmail.com>
2023-01-17 20:30:39 +0100

    First Initial commit

---
+++ src/assets/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+    $(".nav_hover").hover(function () {
+        console.log('Working here');
+        $(this).find('.inner_nav_contents').toggleClass('hidden');
+    });
+
+    // Add click events to show faq group
+    $("#faq div.faq-group").click(function () {
+        let __that = $(this);
+        __that.find('.inner').slideToggle();
+        __that.find('svg').toggleClass('rotate-90');
+    });
+
+
+    $(".open__aside").click(function () {
+        $("aside").slideToggle('slow');
+    });
+
+    $(".close__aside").click(function () {
+        $("aside").slideToggle('fast');
+    });
+});
