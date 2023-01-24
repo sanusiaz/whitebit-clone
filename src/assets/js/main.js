83a0241c56e139aaf5f2e24dd829859f39d875b7
sanusiaz <sanusiabdulazeezadebayo1@gmail.com>
2023-01-24 03:41:01 +0100

    Finshed earn Page Responsiveness

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -12,6 +12,12 @@ $(document).ready(function () {
         __that.find('svg').toggleClass('rotate-90');
     });
 
+    $("#questions .questions_inner_container .card").click(function () {
+        let __that = $(this);
+        __that.find('.inner').slideToggle();
+        __that.find('svg').toggleClass('rotate-45');
+    });
+
 
     $(".open__aside").click(function () {
         $("aside").slideToggle('slow');
@@ -37,39 +43,41 @@ $(document).ready(function () {
         __that.parent().parent().find(`.continent-${__continentValue}`).toggleClass('fill_continent');
     });
 
+    let __slide = $(".splide");
 
+    if ( __slide !== undefined && __slide !== null && __slide.length > 0 ) {
 
+        // configure splide js 
+        var splide = new Splide( '.splide', {
+            perPage: 3,
+            rewind : true,
+          } );
+          
+          splide.mount();
     
     
-    // configure splide js 
-    var splide = new Splide( '.splide', {
-        perPage: 3,
-        rewind : true,
-      } );
-      
-      splide.mount();
-
-
-      document.addEventListener( 'DOMContentLoaded', function () {
-        new Splide('#splide', {
-          type: 'loop',
-          perPage: 3,
-          focus: 'center',
-          autoplay: true,
-          interval: 8000,
-          flickMaxPages: 1,
-          updateOnMove: true,
-          pagination: false,
-          padding: '5%',
-          throttle: 300,
-          breakpoints: {
-            1440: {
-              perPage: 1,
-              padding: '20%'
-            }
-          }
-        }).mount();
-      });
+          document.addEventListener( 'DOMContentLoaded', function () {
+            new Splide('#splide', {
+              type: 'loop',
+              perPage: 3,
+              focus: 'center',
+              autoplay: true,
+              interval: 8000,
+              flickMaxPages: 1,
+              updateOnMove: true,
+              pagination: false,
+              padding: '5%',
+              throttle: 300,
+              breakpoints: {
+                1440: {
+                  perPage: 1,
+                  padding: '20%'
+                }
+              }
+            }).mount();
+          });
+    }
+    
       
     
 });
